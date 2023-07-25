365448e446cd934475697b44f8c2b92f02ccd7ce
Ivan Kopylov <ivan.kopylove@gmail.com>
2023-07-25 16:48:24 +0300

    stabilized tests

--- sandbox-java/src/main/java/com/github/ivan/kopylove/converter/Sum.java
+++
@@ -1,2 +0,0 @@
-package com.github.ivan.kopylove.converter;
-
--- sandbox-maven-spotless/src/test/java/com/github/ivan/kopylove/spotless/MakeSomeCodeMessHere.java
+++ sandbox-maven-spotless/src/test/java/com/github/ivan/kopylove/spotless/MakeSomeCodeMessHere.java
@@ -1,7 +1,6 @@
 package com.github.ivan.kopylove.spotless;
 
-public class MakeSomeCodeMessHere
-{
+public class MakeSomeCodeMessHere {
 
     public void main() {}
 }
