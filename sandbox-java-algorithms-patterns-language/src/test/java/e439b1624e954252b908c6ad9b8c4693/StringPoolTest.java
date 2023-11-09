25475287d37355f3dc99b5d4ad9bde4ddffcd36d
Копылов Иван <kopylov_ia@taximaxim.ru>
2023-11-09 12:24:11 +0300

    post-commit boilerplate, middleware and minor accumulated changes

--- sandbox-java-algorithms-patterns-language/src/test/java/e439b1624e954252b908c6ad9b8c4693/StringPoolTest.java
+++ sandbox-java-algorithms-patterns-language/src/test/java/e439b1624e954252b908c6ad9b8c4693/StringPoolTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 /**
  * Basic demonstration of strings pool in java. Strings are compared intentionally using reference equality.
  */
-@SuppressWarnings({"SimplifiableAssertion", //
+@SuppressWarnings({"SimplifiableAssertion", // using basic mechamism
                    "StringEquality", //
                    "ConstantValue", //
                    "StringConcatenationInLoop"}) // avoid string builder inner logic
