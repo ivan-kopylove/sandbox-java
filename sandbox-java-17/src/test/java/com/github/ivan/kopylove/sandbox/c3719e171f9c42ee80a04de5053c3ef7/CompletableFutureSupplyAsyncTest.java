ca20207861ba1971fed7924e6d0e1b9a6e6feb7b
Копылов Иван <kopylov_ia@taximaxim.ru>
2024-03-07 11:44:40 +0300

    more examples

---
+++ sandbox-java-17/src/test/java/com/github/ivan/kopylove/sandbox/c3719e171f9c42ee80a04de5053c3ef7/CompletableFutureSupplyAsyncTest.java
@@ -0,0 +1,36 @@
+package com.github.ivan.kopylove.sandbox.c3719e171f9c42ee80a04de5053c3ef7;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class CompletableFutureSupplyAsyncTest {
+
+    @Test
+    void exception() {
+        CompletableFuture<String> stringCompletableFuture = CompletableFuture.supplyAsync(() -> {
+            throw new RuntimeException("hello");
+        });
+        assertThrows(RuntimeException.class, () -> {
+            try {
+                stringCompletableFuture.get();
+            } catch (ExecutionException e) {
+                throw e.getCause();
+            }
+        });
+    }
+
+    @Test
+    void return_value() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> stringCompletableFuture = CompletableFuture.supplyAsync(() -> "world");
+
+        fail("try to check yourself before running the assertion");
+        assertThat(stringCompletableFuture.get(), equalTo("world"));
+    }
+}
---
+++ sandbox-java-algorithms-patterns-language/src/test/java/dasjhdkldjlsa/ListNode.kt
@@ -0,0 +1,5 @@
+package dasjhdkldjlsa
+
+class ListNode(var `val`: Int = 0, var next: ListNode? = null) {
+
+}
