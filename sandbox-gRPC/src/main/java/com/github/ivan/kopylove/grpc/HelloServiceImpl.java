a768a10c171df21af195d3abd4f1ef8373521a27
Копылов Иван <kopylov_ia@taximaxim.ru>
2024-04-27 18:19:53 +0300

    upd

--- sandbox-gRPC/src/main/java/com/github/ivan/kopylove/grpc/HelloServiceImpl.java
+++ sandbox-gRPC/src/main/java/com/github/ivan/kopylove/grpc/HelloServiceImpl.java
@@ -1,21 +1,20 @@
 package com.github.ivan.kopylove.grpc;
 
-import io.grpc.stub.StreamObserver;
-
-class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase
-{
-
-    @Override
-    public void hello(HelloRequest request, StreamObserver<HelloResponse> responseObserver)
-    {
-
-        String greeting = "Hello, " + request.getFirstName() + " " + request.getLastName();
-
-        HelloResponse response = HelloResponse.newBuilder()
-                                              .setGreeting(greeting)
-                                              .build();
-
-        responseObserver.onNext(response);
-        responseObserver.onCompleted();
-    }
-}
+//
+//class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase
+//{
+//
+//    @Override
+//    public void hello(HelloRequest request, StreamObserver<HelloResponse> responseObserver)
+//    {
+//
+//        String greeting = "Hello, " + request.getFirstName() + " " + request.getLastName();
+//
+//        HelloResponse response = HelloResponse.newBuilder()
+//                                              .setGreeting(greeting)
+//                                              .build();
+//
+//        responseObserver.onNext(response);
+//        responseObserver.onCompleted();
+//    }
+//}
--- sandbox-gRPC/src/test/java/com/github/ivan/kopylove/grpc/BasicExampleTest.java
+++ sandbox-gRPC/src/test/java/com/github/ivan/kopylove/grpc/BasicExampleTest.java
@@ -1,16 +1,11 @@
 package com.github.ivan.kopylove.grpc;
 
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
 import io.grpc.Server;
-import io.grpc.ServerBuilder;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-
-import static org.hamcrest.CoreMatchers.equalTo;
 
 class BasicExampleTest
 {
@@ -19,9 +14,9 @@ class BasicExampleTest
     @BeforeEach
     public void setup() throws IOException
     {
-        server = ServerBuilder.forPort(8080)
-                              .addService(new HelloServiceImpl())
-                              .build();
+        //        server = ServerBuilder.forPort(8080)
+        //                              .addService(new HelloServiceImpl())
+        //                              .build();
 
         server.start();
     }
@@ -35,19 +30,19 @@ class BasicExampleTest
     @Test
     void base()
     {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
-                                                      .usePlaintext()
-                                                      .build();
-
-        HelloServiceGrpc.HelloServiceBlockingStub stub = HelloServiceGrpc.newBlockingStub(channel);
-
-        HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
-                                                             .setFirstName("Ivan")
-                                                             .setLastName("Kopylov")
-                                                             .build());
-
-        assertThat(helloResponse.getGreeting(), equalTo("Hello, Ivan Kopylov"));
-
-        channel.shutdown();
+        //        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
+        //                                                      .usePlaintext()
+        //                                                      .build();
+        //
+        //        HelloServiceGrpc.HelloServiceBlockingStub stub = HelloServiceGrpc.newBlockingStub(channel);
+        //
+        //        HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
+        //                                                             .setFirstName("Ivan")
+        //                                                             .setLastName("Kopylov")
+        //                                                             .build());
+        //
+        //        assertThat(helloResponse.getGreeting(), equalTo("Hello, Ivan Kopylov"));
+        //
+        //        channel.shutdown();
     }
 }
