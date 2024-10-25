2c563de96c4dc1ffcd93a65e709290acabf91769
Ivan Kopylov <kopylov_ia@taximaxim.ru>
2024-10-25 09:18:47 +0300

    wip

--- sandbox-java-patterns-language-features/src/test/java/a9ae790504a247bbb5c26c05ec46b9e9/RevertedIfConditionTest.java
+++ sandbox-java-patterns-language-features/src/test/java/a9ae790504a247bbb5c26c05ec46b9e9/RevertedIfConditionTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
- * boolean logic playground
+ * Boolean logic playground.
  */
 class RevertedIfConditionTest
 {
