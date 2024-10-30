3bef750c0f93656dde082b1e5a30ddaa4164a2d9
Ivan Kopylov <kopylov_ia@taximaxim.ru>
2024-10-30 11:39:30 +0300

    wip

--- sandbox-spring-security/src/test/java/a8267e6a29d84c338e75b58599527f5d/EncryptionTest.java
+++ sandbox-spring-security/src/test/java/a8267e6a29d84c338e75b58599527f5d/EncryptionTest.java
@@ -1,8 +1,6 @@
 package a8267e6a29d84c338e75b58599527f5d;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
-import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.springframework.security.crypto.encrypt.Encryptors;
 import org.springframework.security.crypto.encrypt.TextEncryptor;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -11,9 +9,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class Test
+public class EncryptionTest
 {
-    @org.junit.jupiter.api.Test
+    @Test
     void abc()
     {
         // given
@@ -26,7 +24,7 @@ public class Test
         assertThat(abc, equalTo("{bcrypt}$2a$10$BQyL02dp7upBfjJve.CYJ.GknphCHkRoz4Ik4U.iW/poOV.5Rt2vi"));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void should_do_something_when_condition()
     {
         // given
