67a8f37e7c4547915f187c92819be3f558e0f046
Ivan Kopylov <kopylov_ia@taximaxim.ru>
2024-11-01 13:22:07 +0300

    wip

---
+++ sandbox-java-patterns-language-features/src/test/java/e74ac6522b1e4cb6a6e6fc8fbe5bb477/ConcurrentLinkedDequeTest.java
@@ -0,0 +1,36 @@
+package e74ac6522b1e4cb6a6e6fc8fbe5bb477;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
+
+public class ConcurrentLinkedDequeTest
+{
+    @Test
+    void should_do_something_when_condition() throws InterruptedException
+    {
+        // given
+        var deque = new ConcurrentLinkedDeque<String>();
+        var threadPool = newFixedThreadPool(200);
+
+        // when
+        IntStream.range(0, 10_000).forEach((value) -> {
+            threadPool.submit(() -> {
+                deque.add(String.valueOf(value));
+            });
+        });
+
+        Thread.sleep(10_000);
+        // then
+        MatcherAssert.assertThat(deque.size(), CoreMatchers.equalTo(10_000));
+    }
+}
