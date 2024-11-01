463275e0bdfe02401954c6433087cb28e738ddf6
Ivan Kopylov <kopylov_ia@taximaxim.ru>
2024-11-01 15:35:34 +0300

    wip

--- sandbox-java-patterns-language-features/src/test/java/a37510d5d96143808112b36a459389d9/IteratingWhilePoppingTest.java
+++ sandbox-java-patterns-language-features/src/test/java/a37510d5d96143808112b36a459389d9/IteratingWhilePoppingTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 /**
  * Removing elements from List<> while iterating leads to exception (btw, which one?), because iterator (... what?)
  * <p>
- * Based on that idea, the experiment about my idea if I can pop elements from the deque while iterating on the que.
+ * Based on that idea, the experiment is about my hypothesis if I can pop elements from the deque while iterating on the que.
  */
 public class IteratingWhilePoppingTest
 {
