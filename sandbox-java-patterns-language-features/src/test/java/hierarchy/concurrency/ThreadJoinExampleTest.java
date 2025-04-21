8c80a4ee7ed44ccd9769678c9ebc97651d7b6139
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-04-21 09:15:05 +0300

    wip snap

--- sandbox-java-patterns-language-features/src/test/java/hierarchy/concurrency/ThreadJoinExampleTest.java
+++ sandbox-java-patterns-language-features/src/test/java/hierarchy/concurrency/ThreadJoinExampleTest.java
@@ -7,13 +7,26 @@ class ThreadJoinExampleTest
 {
         @Test
         @DisplayName("A way to demonstrate that .join operation blocks the execution")
-        void should_do_something_when_condition() throws InterruptedException
+        void should_print_one_by_one_ascending() throws InterruptedException
         {
-
             for(int i = 0; i < 10; i++)
             {
-                MyRunnable t = new MyRunnable(String.valueOf(i));
-                t.getThread().join();
+                int a = i;
+                Thread thread = new Thread(() -> {
+                    System.out.println(a);
+                    try
+                    {
+                        Thread.sleep(500);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        throw new RuntimeException(e);
+                    }
+                });
+//                thread.join(); // try to uncomment the call
+                thread.start();
+                thread.join(); // try to comment the call
+
             }
 
             System.out.println("main thread");
