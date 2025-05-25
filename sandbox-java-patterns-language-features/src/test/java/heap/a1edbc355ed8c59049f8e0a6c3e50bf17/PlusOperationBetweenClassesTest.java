97903d4664c0ba668782d3427fc6171276b5d9e6
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-05-25 16:44:06 +0300

    wip snap

--- sandbox-java-patterns-language-features/src/test/java/heap/a1edbc355ed8c59049f8e0a6c3e50bf17/PlusOperationBetweenClassesTest.java
+++ sandbox-java-patterns-language-features/src/test/java/heap/a1edbc355ed8c59049f8e0a6c3e50bf17/PlusOperationBetweenClassesTest.java
@@ -6,7 +6,7 @@ class PlusOperationBetweenClassesTest
 {
 
     @Test
-    void should_be_resulted_in_compilation_error_when_uncommented()
+    void should_result_in_compilation_error_when_uncommented()
     {
         //        System.out.println(new ClassOne() + new ClassTwo());
     }
--- sandbox-javaagent-hello-world/runner.bat
+++
@@ -1,5 +0,0 @@
-rem compile and place agent jar here
-
-javac Main.java
-java -javaagent:sandbox-javaagent-1.0.jar -cp . Main
-@PAUSE
