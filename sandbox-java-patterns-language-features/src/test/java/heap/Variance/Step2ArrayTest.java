394f60ef650037fde20047f4157f183a1b0f0d9d
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-05-26 16:51:25 +0300

    wip snap

--- sandbox-java-patterns-language-features/src/test/java/heap/Variance/Step2ArrayTest.java
+++ sandbox-java-patterns-language-features/src/test/java/heap/Variance/Step2ArrayTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 /**
  * Answer the questions stated in {@link heap.Variance.Fruit}.
- *
+ * <p>
  * Book search: "is almost certainly a language misfeature".
  */
 public class Step2ArrayTest
