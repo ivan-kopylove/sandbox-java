2e1b741598bc1ce824e9288f4c4b5a256aaf3091
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-05-26 16:13:25 +0300

    wip snap

---
+++ sandbox-java-patterns-language-features/src/test/java/heap/Variance/SignatureTest.java
@@ -0,0 +1,37 @@
+package heap.Variance;
+
+import org.junit.jupiter.api.Test;
+
+class A{
+        public Number test1()
+        {
+                return 1;
+        }
+}
+
+class B extends A
+{
+//        public Integer test2()
+//        {
+//                return 2;
+//        }
+}
+
+public class SignatureTest
+{
+
+
+        @Test
+        void should_do_something_when_condition()
+        {
+//            test1(1);
+//            test1(1.1);
+//            test1(1.1f);
+//            test1(0xab);
+        }
+
+        private void test1(Number number)
+        {
+                System.out.println(number);
+        }
+}
---
+++ sandbox-java-patterns-language-features/src/test/java/heap/Variance/Typed.java
@@ -0,0 +1,43 @@
+package heap.Variance;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Typed
+{
+
+    @Test
+    void test1()
+    {
+        List<Number> nums = Arrays.asList(1, 2);
+//        List<Integer> ints = nums;
+
+        List<Apple> apples = new ArrayList<>();
+//        List<Jonathan> jonathans = apples;
+//        List<Fruit> fruits = apples;
+    }
+
+    void test2()
+    {
+        // apples.add(new Object());
+        // apples.add(new Fruit());
+        // apples.add(new Apple());
+        // apples.add(new Orange());
+        // apples.add(new Jonathan());
+    }
+
+    // what if I uncomment a line?
+    // what PECS it is?
+    void test3(List<Apple> apples)
+    {
+        // apples.add(new Object());
+        // apples.add(new Fruit());
+        // apples.add(new Apple());
+        // apples.add(new Orange());
+        // apples.add(new Jonathan());
+    }
+}
+
